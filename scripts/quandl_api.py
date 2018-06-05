141fa699b3daf70b1fd50dbb888ecc814943e042
Aidin Hassanzadeh <aidin.hassanzadeh@gmail.com>
2018-06-05 17:46:47 -0500

    Add city zip code retriever to 'scripts/quandl_api.py'

--- scripts/quandl_api.py
+++ scripts/quandl_api.py
@@ -1,22 +1,47 @@
 import quandl
+import pandas as pd
+from itertools import product
 from pprint import pprint
+from uszipcode import ZipcodeSearchEngine
 
-API_KEY = "XL_c6FZRjuz6WYRTJB_a"
 
 def get_quandl_code(area_category, area_code, indicator_code):
     return f"ZILLOW/{area_category}{area_code}_{indicator_code}"
 
 
+# init. an ZipcodeSearchEngine instance
+search = ZipcodeSearchEngine()
 
-# set api key
+# define quandl api key
+API_KEY = "XL_c6FZRjuz6WYRTJB_a"
+
+# set cities to query
+cities_states = [dict(state="Texas", city="Austin"),
+                 dict(state="Texas", city="Huston"),
+                 dict(state="Texas", city="Dallas")]
+
+# get zip codes per query city
+df = pd.DataFrame()
+index = 0
+for city_state in cities_states:
+    zip_codes = search.by_city_and_state(state=city_state["state"],
+                                         city=city_state["city"],
+                                         returns=200)
+    for zip_code in zip_codes:
+        df = df.append(pd.DataFrame(zip_code.to_dict(), index=[index]))
+        index += 1
 
 zip_code = "78739"
 area_category = "Z"
 area_code = zip_code
-indicator_code = "ZHVIAH"
 
+# set quandl api key
 quandl.ApiConfig.api_key = API_KEY
-quandl_code = get_quandl_code(area_category, area_code, indicator_code)
 
+# set the indicator code to query
+indicator_code = "ZHVIAH"
+# get quandl code to query
+quandl_code = get_quandl_code(area_category, area_code, indicator_code)
+# get housing in
 data = quandl.get(quandl_code, start_date="1800-01-01", end_date="2018-04-30")
 pprint(data)
