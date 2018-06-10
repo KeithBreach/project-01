451e46dcd8f41c8df7fb72b1fc8728b255941759
vibhu253 <36526050+vibhu253@users.noreply.github.com>
2018-06-10 00:22:26 -0500

    Add Function to retrieve by Zip or Year Data

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,16 @@
 import pandas as pd
 
 
-def get_trend_data(zipcode, df):
+def get_temporal_data(zipcode, df):
+    
+    zip_temporal_df = df.loc[df["Zip Code"]==zipcode,:]
 
+    #trend_df = pd.DataFrame()
+    return zip_temporal_df
 
-    trend_df = pd.DataFrame()
-    return trend_df
+def get_spatial_data(year, df):
+    
+    year_spatial_df = df.loc[df["Year"]==year,:]
+
+    #trend_df = pd.DataFrame()
+    return year_spatial_df
