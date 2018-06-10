ad6ad4887ea4edf257002d06eec7c9b5cfa6eff5
Aidin Hassanzadeh <aidin.hassanzadeh@gmail.com>
2018-06-10 08:44:02 -0500

    Rename notebook files

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,20 @@
 
+from datetime import datetime
 import pandas as pd
-
 
 def get_temporal_data(zipcode, df):
     
-    zip_temporal_df = df.loc[df["Zip Code"]==zipcode,:]
-
-    #trend_df = pd.DataFrame()
+    zip_temporal_df = df.loc[df["Zip Code"] == zipcode, :]
+    # trend_df = pd.DataFrame()
     return zip_temporal_df
+
 
 def get_spatial_data(year, df):
     
-    year_spatial_df = df.loc[df["Year"]==year,:]
-
-    #trend_df = pd.DataFrame()
+    year_spatial_df = df.loc[df["Year"] == year, :]
+    # trend_df = pd.DataFrame()
     return year_spatial_df
+
+
+def parse_time(t):
+    return datetime.strptime(t, '%Y %m %d %H')
