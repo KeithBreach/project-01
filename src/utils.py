9a60ca28d98504fc3382a37e6d144d7e71db620a
Aidin Hassanzadeh <aidin.hassanzadeh@gmail.com>
2018-06-11 22:48:49 -0500

    Add inotebooks/11.1.multi.step.sequence.ltsm.forecast.zhvi.ipynb

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 def get_temporal_data(zipcode, df):
@@ -17,4 +18,29 @@ def get_spatial_data(year, df):
 
 
 def parse_time(t):
+    """
+    :param t:
+    :return:
+    """
     return datetime.strptime(t, '%Y %m %d %H')
+
+
+# create a differenced series
+def create_difference_sequence(sequence, interval=1):
+    """Create difference sequence given input 'sequence' and 'interval'.
+
+    :param sequence:
+    :param interval:
+    :return:
+    """
+
+    diff = list()
+    for i in range(interval, len(sequence)):
+        value = sequence[i] - sequence[i - interval]
+        diff.append(value)
+    return pd.Series(diff)
+
+
+def rescale(x, scale_range=(0,1)):
+    scaler = MinMaxScaler(feature_range=scale_range)
+    return scaler(x), scaler
